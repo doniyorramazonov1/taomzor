4bc75c524b4e2966897a880a6fd57ac532792f40
sharqtechuz <sharqtechuz@gmail.com>
2026-03-30 23:24:03 +0500

    Update: pwa + bot with Supabase config

---
+++ pwa_project/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'taomzor-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+// O'rnatish
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+// Faollashtirish
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+// Tarmoq so'rovlari
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
