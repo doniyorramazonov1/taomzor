a3b3c474ff178c386312655696a069a853207daf
sharqtechuz <sharqtechuz@gmail.com>
2026-03-30 23:48:07 +0500

    Update PWA_URL

--- pwa_project/config.js
+++ pwa_project/config.js
@@ -1,3 +1,3 @@
 const SUPABASE_URL = 'https://qtyfohdvzjgmiqakaeds.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF0eWZvaGR2empnbWlxYWthZWRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxNzAyNTIsImV4cCI6MjA4OTc0NjI1Mn0.M3iAx2-gN0H7m_IT1cDXLN-AmpzsJO5ZCcNJzBByXrs';
-const PWA_URL = 'https://taomzor.uz';
+const PWA_URL = 'https://taomzor-production.up.railway.app';
